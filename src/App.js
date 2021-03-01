3f7012223ea9dfe6bf811460b4f6f6d798918508
Manoel Carvalho <manoel.carvalho@mediacaonline.com>
2021-03-01 15:08:33 -0300

    Branch Master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import react from 'react';
+import { useState, useEffect } from 'react'
+import api from './service/api'
 
 const App = () => {
-  return (
-    <>
-      <span>Maria José</span>
-      <img  src="" />
-    </>
-  
-  );
+    const [user, setUser] = useState('')
+    const [dados, setDados] = useState([])
+
+    useEffect(()=>{
+        const data = api.get('/').then(res => {
+            setDados(res.data)
+        }).catch(err => console.log(err))
+    },[])
+
+    const changeName = (e) => {
+        setUser(e.target.value)
+    }
+    
+    return(
+        <>
+            <h1>{user}</h1>
+            <input type="text" value={user} onChange={changeName}/>
+            <button>Buscar</button>
+        </>
+    );
+
 }
 
 export default App;
